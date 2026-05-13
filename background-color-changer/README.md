ef321345633ae0557e7f4686e96a698ec50b0d8c
Priyanka Kumari <priyanka@Priyankas-MacBook-Air.local>
2026-05-13 17:07:18 +0530

    Initial setup

---
+++ background-color-changer/README.md
@@ -0,0 +1,4 @@
+# React + Vite
+
+This is Background Color changer Project. In which u can chnage the background of app by clicking on button of ur color.
+
---
+++ background-color-changer/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1 className='text-center'>Background Color Changer </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
